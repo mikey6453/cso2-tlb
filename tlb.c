bda44bd2bea9da8c757cd33f463c0362956041e0
Michael <tvq3bn@virginia.edu>
2024-04-05 19:45:50 -0400

    new commit

--- tlb.c
+++ tlb.c
@@ -1,12 +1,12 @@
 #include "tlb.h"
 #include <stddef.h>
-#include <limits.h> 
+#include <limits.h> // For INT_MAX
 
 typedef struct {
-    size_t vpn;      
-    size_t pa;      
-    int valid;         
-    int lru_counter;  
+    size_t vpn;        // Virtual Page Number
+    size_t pa;         // Physical Address
+    int valid;         // Valid bit
+    int lru_counter;   // LRU counter, lower means more recently used
 } TLBEntry;
 
 #define TLB_SETS 16
@@ -17,19 +17,19 @@ void tlb_clear() {
     for (int i = 0; i < TLB_SETS; ++i) {
         for (int j = 0; j < TLB_WAYS; ++j) {
             tlb_cache[i][j].valid = 0;
-            tlb_cache[i][j].lru_counter = INT_MAX; 
+            tlb_cache[i][j].lru_counter = INT_MAX; // Reset LRU counters
         }
     }
 }
 
 void update_lru(int set_index, int accessed_way) {
-    int old_lru = tlb_cache[set_index][accessed_way].lru_counter;
+    int current_lru = tlb_cache[set_index][accessed_way].lru_counter;
     for (int j = 0; j < TLB_WAYS; ++j) {
-        if (tlb_cache[set_index][j].valid && tlb_cache[set_index][j].lru_counter < old_lru) {
+        if (tlb_cache[set_index][j].valid && tlb_cache[set_index][j].lru_counter < current_lru) {
             tlb_cache[set_index][j].lru_counter++;
         }
     }
-    tlb_cache[set_index][accessed_way].lru_counter = 0; 
+    tlb_cache[set_index][accessed_way].lru_counter = 0;
 }
 
 int get_set_index(size_t va) {
@@ -42,7 +42,6 @@ int tlb_peek(size_t va) {
 
     for (int j = 0; j < TLB_WAYS; ++j) {
         if (tlb_cache[set_index][j].valid && tlb_cache[set_index][j].vpn == vpn) {
-            // Return LRU status without changing it
             return TLB_WAYS - tlb_cache[set_index][j].lru_counter;
         }
     }
@@ -54,23 +53,23 @@ size_t tlb_translate(size_t va) {
     size_t page_offset = va & ((1 << POBITS) - 1);
     int set_index = get_set_index(va);
 
-    // Check if entry is in TLB
     for (int j = 0; j < TLB_WAYS; ++j) {
         if (tlb_cache[set_index][j].valid && tlb_cache[set_index][j].vpn == vpn) {
             update_lru(set_index, j);
             return (tlb_cache[set_index][j].pa | page_offset);
         }
     }
+
     size_t new_pa = translate(va & ~((1 << POBITS) - 1));
     if (new_pa == (size_t)-1) {
         return (size_t)-1;
     }
 
-    int lru_way = 0, highest_lru = INT_MIN;
+    int lru_way = 0, max_lru = INT_MIN;
     for (int j = 0; j < TLB_WAYS; ++j) {
-        if (tlb_cache[set_index][j].lru_counter > highest_lru) {
+        if (tlb_cache[set_index][j].lru_counter > max_lru) {
             lru_way = j;
-            highest_lru = tlb_cache[set_index][j].lru_counter;
+            max_lru = tlb_cache[set_index][j].lru_counter;
         }
     }
 
@@ -81,4 +80,3 @@ size_t tlb_translate(size_t va) {
 
     return new_pa | page_offset;
 }
-
